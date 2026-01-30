7d78438fb0fe94ba763ec62685a7855a5fb14eee
Allaassana <144681067+allaassana@users.noreply.github.com>
2026-01-30 16:29:56 -0100

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,9 +1,7 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
 const SUPABASE_URL = "https://SEU_PROJECT_ID.supabase.co";
 const SUPABASE_ANON_KEY = "SUA_ANON_PUBLIC_KEY";
 
-export const supabase = createClient(
+const supabase = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
