2d8e63b1729d74d4378dd03d06f8f24eaeaed3de
Allaassana <144681067+allaassana@users.noreply.github.com>
2026-01-31 11:50:23 -0100

    Delete js/supabase.js

--- js/supabase.js
+++
@@ -1,9 +0,0 @@
-// js/supabase.js
-
-const SUPABASE_URL = "https://SEU_PROJECT_ID.supabase.co";
-const SUPABASE_ANON_KEY = "SUA_ANON_PUBLIC_KEY";
-
-window.supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
