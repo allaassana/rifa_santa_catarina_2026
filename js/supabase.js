f248bb40626d7b2dcc416407e4250bd8515c6096
Allaassana <144681067+allaassana@users.noreply.github.com>
2026-01-31 11:07:23 -0100

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -3,7 +3,7 @@
 const SUPABASE_URL = "https://SEU_PROJECT_ID.supabase.co";
 const SUPABASE_ANON_KEY = "SUA_ANON_PUBLIC_KEY";
 
-const supabase = window.supabase.createClient(
+window.supabaseClient = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
